6e0dd49865e70b520e263f7734ad3c7c3d8d8eeb
renecapella <renecapella.dev@gmail.com>
2022-02-14 21:17:09 -0800

    Week 6 AD6, adding portfolio site to flask project

--- AD6/Flasker.py
+++ AD6/Flasker.py
@@ -2,23 +2,26 @@
 # . venv/bin/activate
 # pip install Flask
 # export FLASK_APP=Flasker
+# flask run
 
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/hello_user/<user>')
-def hello_name(user):
-   return render_template('hello_user.html', name = user)
+@app.route('/')
+def home():
+   return render_template('home.html')
 
-@app.route('/hello/<int:score>')
-def hello_score(score):
-   return render_template('hello.html', marks = score)
+@app.route('/portfolio')
+def portfolio():
+   return render_template('portfolio.html')
 
-@app.route('/result')
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/contact')
 def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
-
+   return render_template('contact.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
