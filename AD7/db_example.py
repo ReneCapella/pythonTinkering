180eec73be1ba84cec043321182c1b8119cd0819
renecapella <renecapella.dev@gmail.com>
2022-02-15 20:20:42 -0800

    Week 7 set up for AD7

---
+++ AD7/db_example.py
@@ -0,0 +1,13 @@
+# With Python and SQLite -
+
+# Create a test database.
+# Create a table in the test database.
+# Add some records to the table.
+# Search the table for a specific record.
+# Update a record in the table.
+# Delete a record from the table.
+
+# As a deliverable for this assignment, screen shots and/or text file copies of
+# the queries above will be enough. If you get the proper query results, I will
+# assume your Python code is working well. (For troubleshooting requests, please
+# also upload the code!)
